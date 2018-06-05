56e502daf4500a17d3d4d08a847b3d09ea4b80b7
jobran <jobran41@gmail.com>
2018-06-05 21:20:49 +0100

    add proptype

--- src/components/sideBar/SideBar.js
+++ src/components/sideBar/SideBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react"
+import PropTypes from "prop-types"
 import { LinearProgress, Avatar, Button } from "react-md"
 
 import Logo from "images/logo.png"
@@ -6,30 +7,61 @@ import Logo from "images/logo.png"
 import "./SideBar.scss"
 
 //todo proptype with action information and Edit
-export default class SideBar extends Component {
+class SideBar extends Component {
+  logout = () => {
+    const { logout } = this.props
+    if (typeof logout === 'function') {
+      logout()
+    }
+  }
+  renderBadge = () => {
+    const {
+      badget,
+    } = this.props.data
+    if (Array.isArray(badget)) {
+      let badgetLocal = badget.map((item, i) => {
+        return (
+          <Avatar key={i} className="SideBar-budges-avatar">
+            {" "}
+            {item}
+          </Avatar>
+        )
+      })
+      return badgetLocal
+    } else {
+      return []
+    }
+  }
+
+  renderSocial = () => {
+    const {
+      social,
+    } = this.props.data
+    if (Array.isArray(social)) {
+      let socialLocal = social.map((item, i) => {
+        return <li key={i}>{item}</li>
+      })
+      return socialLocal
+    } else {
+      return []
+    }
+  }
+
+  isEmptyObject = (obj) => {
+    for (let key in obj) {
+      if (obj.hasOwnProperty(key)) return false
+    }
+    return true
+  }
   render() {
     const {
       profileName,
       skillsName,
       tel,
       Email,
-      social,
-      badget,
       grade,
       ProgressBar: { level, indicatorMax, indicatorMin }
     } = this.props.data
-    const { logout } = this.props
-    const socialLocal = social.map((item, i) => {
-      return <li key={i}>{item}</li>
-    })
-    const badgetLocal = badget.map((item, i) => {
-      return (
-        <Avatar key={i} className="SideBar-budges-avatar">
-          {" "}
-          {item}
-        </Avatar>
-      )
-    })
     return (
       <div className="SideBar">
         <div className="SideBar-header">
@@ -44,25 +76,31 @@ export default class SideBar extends Component {
               <p className="Email">{Email}</p>
             </div>
           </div>
-          <LinearProgress
-            id="progress-style-example"
-            value={level}
-            query
-            className="horizontale-progress"
-            // style={{ height: 100, width: 20 }}
-            //progressClassName="horizontale-progress"
-            progressStyle={value => ({ top: `${100 - value}%`, width: "100%" })}
-          />
-          <div className="LinearProgress-details">
-            <span>{indicatorMin}</span>
-            <span>{indicatorMax}</span>
-          </div>
+          {!this.isEmptyObject(this.props.data.ProgressBar) && typeof this.props.data.ProgressBar === 'object' &&
+            <div>
+              <LinearProgress
+                id="progress-style-example"
+                value={level || 0}
+                query
+                className="horizontale-progress"
+                // style={{ height: 100, width: 20 }}
+                //progressClassName="horizontale-progress"
+                progressStyle={value => ({ top: `${100 - value}%`, width: "100%" })}
+              />
+              <div className="LinearProgress-details">
+                <span>{indicatorMin}</span>
+                <span>{indicatorMax}</span>
+              </div>
+            </div>
+          }
+
+
         </div>
         <div className="SideBar-body">
-          <ul className="socialLink">{socialLocal}</ul>
+          <ul className="socialLink">{this.renderSocial()}</ul>
           <div className="budges">
             <div className="budges-header">Badges</div>
-            {badgetLocal}
+            {this.renderBadge()}
           </div>
           <div className="grade">Grade</div>
           <p>{grade}</p>
@@ -75,7 +113,7 @@ export default class SideBar extends Component {
             Information
           </Button>
           <Button
-            onClick={logout}
+            onClick={this.logout}
             flat
             iconBefore={false}
             iconClassName="mdi mdi-home"
@@ -87,3 +125,16 @@ export default class SideBar extends Component {
     )
   }
 }
+
+SideBar.propTypes = {
+  ProgressBar: PropTypes.object,
+  logout: PropTypes.func,
+  profileName: PropTypes.string,
+  skillsName: PropTypes.string,
+  Email: PropTypes.string,
+  social: PropTypes.array,
+  badget: PropTypes.array
+
+}
+
+export default SideBar
