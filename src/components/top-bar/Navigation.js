49fee7f401cc39cfdbdb14d5001ba6182b27e08c
jobran <jobran41@gmail.com>
2018-06-05 12:28:35 +0100

    delete Link from navigation component

--- src/components/top-bar/Navigation.js
+++ src/components/top-bar/Navigation.js
@@ -1,48 +1,73 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { Button } from "react-md";
 import { connect } from "react-redux";
+import { withRouter } from "react-router";
 
 import * as routes from "libs/constants/routes";
 
-const Navigation = ({ authUser }) => (
-  <div className="Navigation">
-    {authUser ? <NavigationAuth /> : <NavigationNonAuth />}
-  </div>
-);
+import React, { Component } from "react";
 
-const NavigationAuth = () => (
-  <ul>
-    <li>
-      <Link to={`${routes.Container}${routes.Dashboard}`}>Dashboard</Link>
-    </li>
-    <li>
-      <Link to={`${routes.Container}${routes.Aquarium}`}>Aquarium</Link>
-    </li>
-    <li>
-      <Link to={`${routes.Container}${routes.Form}`}>Form</Link>
-    </li>
-    <li>
-      <Link to={`${routes.Container}${routes.Lab}`}>Lab</Link>
-    </li>
-    <li>
-      <Link to={`${routes.Container}${routes.Search}`}>Search</Link>
-    </li>
-  </ul>
-);
+@withRouter
+@connect(({ userState }) => ({
+  authUser: userState.users
+}))
+class Navigation extends Component {
+  pushurl = url => {
+    const { history } = this.props;
+    history.push(url);
+  };
+  NavigationAuth = () => (
+    <div className="menu-withLogin">
+      <Button
+        onClick={() => this.pushurl(`${routes.Container}${routes.Dashboard}`)}
+        raised
+      >
+        Dashboard
+      </Button>
+      <Button
+        onClick={() => this.pushurl(`${routes.Container}${routes.Aquarium}`)}
+        raised
+      >
+        Aquarium
+      </Button>
+      <Button
+        onClick={() => this.pushurl(`${routes.Container}${routes.Lab}`)}
+        raised
+      >
+        Lab
+      </Button>
+      <Button
+        onClick={() => this.pushurl(`${routes.Container}${routes.Form}`)}
+        raised
+      >
+        Form
+      </Button>
+      <Button
+        onClick={() => this.pushurl(`${routes.Container}${routes.Search}`)}
+        raised
+      >
+        Search
+      </Button>
+    </div>
+  );
 
-const NavigationNonAuth = () => (
-  <ul>
-    <li>
-      <Link to={routes.LANDING}>Landing</Link>
-    </li>
-    <li>
-      <Link to={routes.SIGN_IN}>Sign In</Link>
-    </li>
-  </ul>
-);
+  NavigationNonAuth = () => (
+    <div className="menu-withoutLogin">
+      <Button onClick={() => this.pushurl(`${routes.SIGN_IN}`)} raised>
+        SIGN_IN
+      </Button>
+      <Button onClick={() => this.pushurl(`${routes.SIGN_UP}`)} raised>
+        SIGN_UP
+      </Button>
+    </div>
+  );
+  render() {
+    const { authUser } = this.props;
+    return (
+      <div className="Navigation">
+        {authUser ? this.NavigationAuth() : this.NavigationNonAuth()}
+      </div>
+    );
+  }
+}
 
-const mapStateToProps = state => ({
-  authUser: state.userState.users
-});
-
-export default connect(mapStateToProps)(Navigation);
+export default Navigation;
